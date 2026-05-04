39c86b2cef7ba32e43cc1fc6254212a9555fe6f2
aliyahhxd <micaaliyahhxd@gmail.com>
2026-05-04 14:52:52 +0800

    auuuuuth!

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,42 +1,42 @@
-    <?php
+<?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Auth;
-    use App\Models\User;
-    use Log;
-    //TODO : Implement login functionality
-    class AuthController extends Controller
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Log;
+//TODO : Implement login functionality
+class AuthController extends Controller
+{
+    public function login(Request $request)
     {
-        public function login(Request $request)
-        {
-            $request->validate([
-                'email' => 'required|email',
-                'password' => 'required'
-            ]);
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
 
-            if (!Auth::attempt($request->only('email', 'password'))) {
-                return response()->json([
-                    'message' => 'Invalid credentials'
-                ], 401);
-            }
-
-            $user = User::where('email', $request->email)->first();
-            $token = $user->createToken('auth_token')->plainTextToken;
-
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response()->json([
-                'message' => 'Login successful',
-                'token' => $token,
-                'user' => [
-                    'id' => $user->id,
-                    'first_name' => $user->first_name,
-                    'last_name' => $user->last_name,
-                    'email' => $user->email,
-                    'role' => $user->role
-                ]
-            ], 200);
+                'message' => 'Invalid credentials'
+            ], 401);
         }
 
-        
+        $user = User::where('email', $request->email)->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => [
+                'id' => $user->id,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'email' => $user->email,
+                'role' => $user->role
+            ]
+        ], 200);
     }
+
+    
+}
